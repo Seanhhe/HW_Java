c0ef64ce0c6d73df30fb531ab8307867e9825eb0
dh-46 <danielhuang.work@gmail.com>
2018-07-26 19:14:50 +0800

    Laptop Practice

---
+++ Practice_Java/src/tw/org/iii/practiceJava/Helloworld2.java
@@ -0,0 +1,11 @@
+package tw.org.iii.practiceJava;
+
+public class Helloworld2 {
+
+	public static void main(String[] args) {
+		// TODO Todo 可在tasks功能類似快捷鍵方式使用
+		System.out.println("Hello, world2");
+
+	}
+
+}
---
+++ Practice_Java/src/tw/org/iii/practiceJava/Practice001.java
@@ -0,0 +1,9 @@
+package tw.org.iii.practiceJava;
+
+public class Practice001 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+
+}
