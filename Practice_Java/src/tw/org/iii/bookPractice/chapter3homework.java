9b846e865e6ebb1e0d8927e58a79f72a0006da19
dh-46 <danielhuang.work@gmail.com>
2018-07-27 17:09:09 +0800

    add tw.org.iii.bookPractice

---
+++ Practice_Java/src/tw/org/iii/bookPractice/chapter3homework.java
@@ -0,0 +1,13 @@
+package tw.org.iii.bookPractice;
+
+public class chapter3homework {
+
+	public static void main(String[] args) {
+		//	1. 如果有 m, n 兩個變數, 分別儲存 1000, 495 兩個值, 請使用程式算出最大公因數.
+		
+		
+		//	2. 在三位數的整數中, 例如153可以滿足 1^3 + 5^3 + 3^3 = 153, 這樣的阿姆斯壯數, 試以程式找出所有三位數的阿姆斯壯數.
+
+	}
+
+}
--- Practice_Java/src/tw/org/iii/practiceJava/Homework01.java
+++ Practice_Java/src/tw/org/iii/practiceJava/Homework01.java
@@ -5,7 +5,6 @@ public class Homework01 {
 	public static void main(String[] args) {
 		// TODO 萬年曆程式
 		
-		//	
 
 	}
 
