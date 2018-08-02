731e4a05a31c994b4f7bdb3f1dc5c93a435d0b43
dh-46 <danielhuang.work@gmail.com>
2018-08-02 11:12:46 +0800

    20180802 AM reviewed

--- Practice_Java/src/tw/org/iii/practiceJava/Reviewed020_Poker_v5.java
+++ Practice_Java/src/tw/org/iii/practiceJava/Reviewed020_Poker_v5.java
@@ -1,5 +1,7 @@
 package tw.org.iii.practiceJava;
 
+import java.util.Arrays;
+
 public class Reviewed020_Poker_v5 {
 
 	public static void main(String[] args) {
@@ -9,6 +11,28 @@ public class Reviewed020_Poker_v5 {
 		 * 	一副牌-->52張牌 (一維陣列)
 		 * 
 		 * 	version5: 發牌&理牌
+		 * 	發牌->發給四個玩家,每人13張牌 =>二維陣列
+		 * 	
+		 * 	i%4 (x軸: 玩家)  i/4 (y軸:玩家牌的順序)
+		 * 	0 1 2 3    0 0 0 0
+		 * 	4 5 6 7    1 1 1 1
+		 * 	8 9 10 11  2 2 2 2
+		 * 	........
+		 * 	
+		 * 
+		 * 	加上花色->加上花色與大小
+		 * 
+		 * 	黑桃1 黑桃2 紅心2 方塊5 梅花1 梅花3
+		 * 
+		 * 	0~12 黑桃   [ /13 0...0,1,2,3 ]  
+			13~25 紅心 [ /13 1...0,1,2,3 ]
+		 	26~38 方塊 [ /13 1...0,1,2,3 ]
+			39~51 梅花 [ /13 1...0,1,2,3 ]
+			原始編號除以13的商 代表花色; 餘數代表牌數
+		 * 	
+		 * 	理牌->由大到小排順序
+		 * 	利用Arrays.sort API
+		 * 
 		 */
 		
 		//	產生新撲克牌的陣列
@@ -19,7 +43,7 @@ public class Reviewed020_Poker_v5 {
 		}
 		
 		System.out.println();
-		System.out.println("--------");
+		System.out.println("----洗牌----");
 		
 		//	交換
 		
@@ -36,7 +60,43 @@ public class Reviewed020_Poker_v5 {
 		for (int finalCard : cards) {
 			System.out.print(finalCard+ ",");
 		}
+		
+		System.out.println();
+		System.out.println("--------發牌---------");
+		
+		// 發牌
+		int[][] players = new int [4][13];
+		
+		for(int i = 0; i < cards.length;i++) {	// for 迴圈存取每一張洗好的牌
+			//	i%4->確認i張牌是給哪一個玩家
+			//	i/4->確認i張牌是給在該玩家的哪一個位置
+			players[i%4][i/4] = cards[i];
+		}
+		
+		//	用for each 檢查每個人的牌
+		for (int[] player: players) {
+			for(int eachCards : player) {
+				System.out.print(eachCards+"\t");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		System.out.println("--------加上花色&理牌---------");
+		
+		//	加上花色&理牌
 				
+		String[] title = new String[] {"黑桃","紅心","方塊","梅花"};
+		String[] numStr = new String[] {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+		
+		for (int[] player: players) {
+			Arrays.sort(player);
+			for(int eachCards : player) {
+				System.out.print(title[eachCards/13]+numStr[eachCards%13] +"\t");
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
---
+++ Practice_Java/src/tw/org/iii/practiceJava/Reviewed022_Objects.java
@@ -0,0 +1,14 @@
+package tw.org.iii.practiceJava;
+
+public class Reviewed022_Objects {
+
+	public static void main(String[] args) {
+		// Brad21 20180729AM
+		/*
+		 * 物件介紹
+		 */
+		
+		
+	}
+
+}
