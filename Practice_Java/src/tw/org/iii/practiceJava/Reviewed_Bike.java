757d01e515eabff77c289fefd30446f69aa3e339
dh-46 <danielhuang.work@gmail.com>
2018-08-02 11:58:12 +0800

    20180802 Review

---
+++ Practice_Java/src/tw/org/iii/practiceJava/Reviewed_Bike.java
@@ -0,0 +1,20 @@
+package tw.org.iii.practiceJava; //執行環境所在的實體"路徑"
+
+/*20180729AM 物件 Bike
+ * 以Bike為例建立物件
+ * 這個檔案是建立Bike類別
+ * */
+
+public class Reviewed_Bike {
+	
+	//	宣告屬性
+	double speed;
+	
+	//	建構式
+	Reviewed_Bike() { // 建構式的初始化.
+
+	}
+	
+	//	方法
+
+}
