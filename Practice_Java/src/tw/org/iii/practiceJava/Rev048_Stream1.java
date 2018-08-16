749824daa210f354ffed78849fde8ca93b5fe8ee
dh-46 <danielhuang.work@gmail.com>
2018-08-16 20:50:57 +0800

    20180816PM

--- Practice_Java/src/tw/org/iii/practiceJava/Rev048_Stream1.java
+++ Practice_Java/src/tw/org/iii/practiceJava/Rev048_Stream1.java
@@ -1,12 +1,23 @@
 package tw.org.iii.practiceJava;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 /*	20180811PM2 17:00 Brad46 串流Part1
  * 	INPUT STREAM
- * 
+ * 	
+ * 	java.io.InputStream
+ * 		java.io.FileInputStream
  */
 public class Rev048_Stream1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			FileInputStream fin = new FileInputStream("dir1/Rev.txt");
+			
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
 
 	}
 
