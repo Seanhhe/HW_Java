039c06ce1d4f1cf0d3e812c41a77d436ae157756
dh-46 <danielhuang.work@gmail.com>
2018-09-25 16:11:16 +0800

    20180925

--- Practice_Java/src/p1/Rev01.java
+++ Practice_Java/src/p1/Rev01.java
@@ -3,6 +3,8 @@ package p1;
  * 20180811AM2 Package
  */
 public class Rev01 {
+	private static int a = 10;
+	private int b = 10;
 	void m1() {
 		//Rev02 obj1 = new Rev02(); // 呼叫不到不同package的Lab2
 	}
@@ -10,5 +12,13 @@ public class Rev01 {
 	protected void m2() {
 		// 存取修飾字為protected
 		// -->相同package或為父子類別可看的到
+	}
+	
+	public static void main(String[] args) {
+		a++;
+		Rev01 r1 = new Rev01();
+		r1.b++;
+		System.out.println(a);
+		System.out.println(r1.b);
 	}
 }
